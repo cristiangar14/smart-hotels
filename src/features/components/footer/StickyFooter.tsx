fb05cb5f4011650718b13a4bf915c3dc5f5737cc
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-18 19:19:41 -0500

    first footer

---
+++ src/features/components/footer/StickyFooter.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+// our component
+import CopyRight from '../copyRight/CopyRight';
+
+const StickyFooter = () => {
+  return (
+    <Box sx={
+        {
+            display:'flex',
+            flexDirection: 'column',
+        }
+    }
+    >
+        <CssBaseline />
+        <Box
+            component='footer'
+            sx={
+                {
+                    py: 3,
+                    px: 2,
+                    mt: 'auto',
+                    backgroundColor: (theme) => theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800]
+                }
+            }
+        >
+            <Container maxWidth='sm'>
+                <CopyRight sx={
+                    {
+                        pt:4
+                    }
+                } />
+            </Container>
+        </Box>
+
+    </Box>
+  )
+}
+
+export default StickyFooter
