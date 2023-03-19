1c2ae7387917e3b016a1a77cd2d2783a0adf5733
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-18 19:17:16 -0500

    starting with redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import hotelListReducer from '../features/hotelList/hotelListSlice';
+import authReducer from '../features/login/loginSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    hotelList: hotelListReducer,
+    auth: authReducer,
   },
 });
 
