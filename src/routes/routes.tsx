5aeced6d96ce687c2b672963d76df0be446c4310
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-18 19:17:58 -0500

    statring with react-router-dom

---
+++ src/routes/routes.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import { Dashboard } from "../features/dashboard/Dashboard";
+import { Login } from "../features/login/Login";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/reservation",
+    element: <div>Form for reservation</div>,
+  },
+  {
+    path: "/editRoom",
+    element: <div>Form for editing room</div>,
+  },
+  {
+    path: "/bookings",
+    element: <div>Form for create bookings</div>,
+  },
+]);
