1530f66830de60e78395870e6ba1374873b541de
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-28 20:40:12 -0500

    clianing

--- src/features/hotelList/hotelListAPI.ts
+++ src/features/hotelList/hotelListAPI.ts
@@ -1,6 +1,5 @@
 import { Hotel } from "../../utils/types/hotel.types";
 
-// A mock function to mimic making an async request for data
 export function fetchHotels() {
   return new Promise<{ data: Hotel[] }>((resolve) =>
     setTimeout(() => resolve({ data: [{_id:'dsf1ad2f1', name: 'Hotel 1', numberRooms: 40}] }), 500)
